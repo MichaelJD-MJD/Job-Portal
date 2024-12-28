6ba353908592b86871dde4b631854a7239411bfc
Michael JD <michaeljdarwin9@gmail.com>
2024-12-28 10:46:24 +0700

    feat: recruiter login pop up

--- client-job-portal/src/App.jsx
+++ client-job-portal/src/App.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import {Route, Routes} from 'react-router-dom'
 import Home from "./pages/Home";
 import ApplyJob from "./pages/ApplyJob";
 import Application from "./pages/Application";
+import RecruiterLogin from "./components/RecruiterLogin";
+import { AppContext } from "./context/AppContext";
 
 const App = () => {
-  return <div>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/apply-job/:id" element={<ApplyJob />} />
-      <Route path="/applications" element={<Application />} />
-    </Routes>
-  </div>;
+
+  const {showRecruiterLogin} = useContext(AppContext);
+
+  return (
+    <div>
+      {showRecruiterLogin && <RecruiterLogin />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Application />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
