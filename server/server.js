7c6f847f54c8c5dc14f9b2917934e6b089944cd0
Michael JD <michaeljdarwin9@gmail.com>
2024-12-30 14:59:10 +0700

    setup sentry

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js';
-
+import * as Sentry from "@sentry/node";
 
 // Initialize Express
 const app = express();
@@ -16,9 +17,16 @@ app.use(express.json()); // ini supaya respons yg diberikan dalam bentuk json
 
 // Routes
 app.get('/', (req, res)=> res.send("Api working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
 
 // Port 
 const PORT = process.env.PORT || 5000
+
+// Setup sentry
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
