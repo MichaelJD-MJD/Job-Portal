a02c5b24357fef010094f9437979020a109b72d7
MichaelJD-MJD <138199815+MichaelJD-MJD@users.noreply.github.com>
2024-12-30 22:35:49 +0700

    Dev (#2)
    * feat: setup route
    
    * feat: setup context
    
    * feat: make navbar component
    
    * feat: using clerk for authentication
    
    * feat: make hero component
    
    * feat: make filter and job listing
    
    * feat: responsive and pagination
    
    * feat: appDownload and footer component
    
    * feat: add filter logic
    
    * feat: apply job page
    
    * feat: application page
    
    * feat: recruiter login pop up
    
    * feat: add page, route for recruiter and navbar recruiter
    
    * feat: sidebar in dashboard panel
    
    * feat: add job page
    
    * feat: view applications page
    
    * feat: manage jobs page
    
    * setup backend
    
    * add configuration for server and connect to mongoDB
    
    * setup sentry
    
    * feat: create model for user and webhook controller

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import './config/instrument.js'
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js';
+import * as Sentry from "@sentry/node";
+import { clerkWebHooks } from './controllers/webhooks.js';
+
+// Initialize Express
+const app = express();
+
+// Connect to Database
+await connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); // ini supaya respons yg diberikan dalam bentuk json
+
+// Routes
+app.get('/', (req, res)=> res.send("Api working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+app.post("/webhooks", clerkWebHooks);
+
+
+// Port 
+const PORT = process.env.PORT || 5000
+
+// Setup sentry
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
