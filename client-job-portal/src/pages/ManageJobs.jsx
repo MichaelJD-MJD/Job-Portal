6116432bee4407ce43af05274d1cff53b813fc01
Michael JD <michaeljdarwin9@gmail.com>
2025-01-02 13:57:03 +0700

    feat: implement API in manage jobs page

--- client-job-portal/src/pages/ManageJobs.jsx
+++ client-job-portal/src/pages/ManageJobs.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
-import { manageJobsData } from '../assets/assets';
-import moment from 'moment'
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { manageJobsData } from "../assets/assets";
+import moment from "moment";
+import { data, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const ManageJobs = () => {
-
   const navigate = useNavigate();
+
+  const [jobs, setJobs] = useState([]);
+
+  const { backendUrl, companyToken } = useContext(AppContext);
+
+  // Function to fetch company job application data
+  const fetchCompanyJobs = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/company/list-jobs", {
+        headers: { token: companyToken },
+      });
+
+      if (data.success) {
+        setJobs(data.jobsData.reverse());
+        console.log(data.jobsData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Function to change Job Visibility
+  const changeJobVisibility = async (id) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/company/change-visibility",
+        { id, },
+        {
+          headers: { token: companyToken },
+        }
+      );
+
+      if(data.success) {
+        toast.success(data.message);
+        fetchCompanyJobs()
+      }else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
+
+  useEffect(() => {
+    if (companyToken) {
+      fetchCompanyJobs();
+    }
+  }, [companyToken]);
 
   return (
     <div className="container p-4 max-w-5xl">
@@ -26,7 +79,7 @@ const ManageJobs = () => {
             </tr>
           </thead>
           <tbody>
-            {manageJobsData.map((job, index) => (
+            {jobs.map((job, index) => (
               <tr key={index} className="text-gray-700">
                 <td className="py-2 px-4 border-b max-sm:hidden">
                   {index + 1}
@@ -38,20 +91,32 @@ const ManageJobs = () => {
                 <td className="py-2 px-4 border-b max-sm:hidden">
                   {job.location}
                 </td>
-                <td className="py-2 px-4 border-b text-center">{job.applicants}</td>
+                <td className="py-2 px-4 border-b text-center">
+                  {job.applicants}
+                </td>
                 <td className="py-2 px-4 border-b">
-                  <input className='scale-125 ml-4' type="checkbox" />
+                  <input
+                    onChange={() => changeJobVisibility(job._id)}
+                    className="scale-125 ml-4"
+                    type="checkbox"
+                    checked={job.visible}
+                  />
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <div className='mt-4 flex justify-end'>
-        <button onClick={() => navigate("/dashboard/add-job")} className='bg-black text-white py-2 px-4 rounded'>Add new job</button>
+      <div className="mt-4 flex justify-end">
+        <button
+          onClick={() => navigate("/dashboard/add-job")}
+          className="bg-black text-white py-2 px-4 rounded"
+        >
+          Add new job
+        </button>
       </div>
     </div>
   );
-}
+};
 
-export default ManageJobs
+export default ManageJobs;
--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
 import Job from "../models/Job.js";
+import JobApplications from "../models/JobApplications.js";
 
 // Register a new company
 export const registerCompany = async (req, res) => {
@@ -126,9 +127,13 @@ export const getCompanyPostedJobs = async (req, res) => {
 
         const jobs = await Job.find({companyId});
 
-        // {TODO} adding no of applicatns info in data
+        // adding no of applicatns info in data
+        const jobsData = await Promise.all(jobs.map(async(job) => {
+          const applicants = await JobApplications.find({jobId: job._id})
+          return {...job.toObject(), applicants: applicants.length}
+        }))
 
-        res.json({success: true, jobsData: jobs})
+        res.json({success: true, jobsData})
 
     } catch (error) {
         res.json({ success: false, message: error.message });
