216dbced20cdc5eddd9bd4db19baa199846f14c5
Michael JD <michaeljdarwin9@gmail.com>
2025-01-02 09:23:13 +0700

    feat: add some routes, controller, model, middleware, util, and config

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ import 'dotenv/config'
 import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node";
 import { clerkWebHooks } from './controllers/webhooks.js';
+import companyRoutes from './routes/companyRoutes.js';
+import connectCloudinary from './config/cloudinary.js';
+import jobRoutes from './routes/jobRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import {clerkMiddleware} from '@clerk/express';
 
 // Initialize Express
 const app = express();
@@ -12,9 +17,13 @@ const app = express();
 // Connect to Database
 await connectDB();
 
+// connect to cloudinary
+await connectCloudinary()
+
 // Middlewares
 app.use(cors());
 app.use(express.json()); // ini supaya respons yg diberikan dalam bentuk json
+app.use(clerkMiddleware());
 
 // Routes
 app.get('/', (req, res)=> res.send("Api working"))
@@ -22,7 +31,9 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 app.post("/webhooks", clerkWebHooks);
-
+app.use('/api/company', companyRoutes);
+app.use('/api/jobs', jobRoutes);
+app.use('/api/users', userRoutes);
 
 // Port 
 const PORT = process.env.PORT || 5000
